e3d1a4ed491ba68eecc94b7bc8fe15b7aec05abf
Tim Sogard <src@timsogard.com>
2019-10-20 00:04:25 -0400

    Add filtering to GetRecentPosts
    Laying groundwork for filters. Using structs for each filter type as it
    feels the least lazy. Will reconsider if it gets unwieldy.

--- posts.go
+++ posts.go
@@ -2,9 +2,10 @@ package pinboard
 
 import (
 	"encoding/xml"
+	"fmt"
 	"io/ioutil"
-	//"log"
 	"net/http"
+	"net/url"
 	"time"
 )
 
@@ -27,10 +28,15 @@ type Post struct {
 }
 
 type PostFilter struct {
-	tags []string
-	dt   time.Time
-	url  string
-	meta bool
+	Tags []string
+	Dt   time.Time
+	Url  string
+	Meta bool
+}
+
+type RecentPostsFilter struct {
+	Tags  []string
+	Count int
 }
 
 func ParseResponse(resp *http.Response) ([]Post, error) {
@@ -48,20 +54,42 @@ func ParseResponse(resp *http.Response) ([]Post, error) {
 }
 
 func (p *Pinboard) GetPosts(pf PostFilter) ([]Post, error) {
-	url := "https://api.pinboard.in/v1/posts/get"
-	resp, err := p.Get(url)
+	u, _ := url.Parse(APIBase + "posts/get")
+	resp, err := p.Get(u.String())
 	if err != nil {
 		return nil, err
 	}
 	return ParseResponse(resp)
 }
 
-func (p *Pinboard) GetRecentPosts() ([]Post, error) {
-	url := "https://api.pinboard.in/v1/posts/recent"
-	resp, err := p.Get(url)
+func (p *Pinboard) GetRecentPosts(rpf RecentPostsFilter) ([]Post, error) {
+	u, err := url.Parse(APIBase + "posts/recent")
+
+	// Filters
+	q := u.Query()
+	if rpf.Count != 0 {
+		if rpf.Count < 0 || rpf.Count > 100 {
+			return nil, fmt.Errorf("RecentPostsFilter count must be between 0 and 100")
+		}
+		q.Set("count", fmt.Sprintf("%d", rpf.Count))
+	}
+
+	if len(rpf.Tags) > 0 {
+		if len(rpf.Tags) > 3 {
+			return nil, fmt.Errorf("RecentPostsFilter cannot accept more than 3 tags")
+		}
+		for _, t := range rpf.Tags {
+			q.Add("tag", t)
+		}
+	}
+	u.RawQuery = q.Encode()
+
+	// Get posts
+	resp, err := p.Get(u.String())
 	if err != nil {
 		return nil, err
 	}
+
 	return ParseResponse(resp)
 }
 
