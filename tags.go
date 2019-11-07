68f18d3b6cc5ab5ca748909f593a940a8d703418
Tim Sogard <src@timsogard.com>
2019-11-07 04:41:59 -0500

    Add DeleteTag
    DeleteTag (like other Pinboard delete calls) doesn't return a meaningful
    response (in fact for tags/delete it returns an XML header and a JSON
    body). DeleteTag only returns an error if an HTTP error occurs.

--- tags.go
+++ tags.go
@@ -48,6 +48,32 @@ func (p *Pinboard) GetTags() (Tags, error) {
 	return ParseTagsResponse(resp)
 }
 
+// DeleteTag deletes the given tag from a user's Pinboard account. There is no
+// central store for tags, they are simply removed from every post in a user's
+// account. This API endpoint has no meaningful response, so an error is returned
+// only if the HTTP request fails.
+func (p *Pinboard) DeleteTag(tag string) error {
+	u, err := url.Parse(APIBase + "tags/delete")
+	if err != nil {
+		return fmt.Errorf("Failed to parse DeleteTag API URL: %v", err)
+	}
+	q := u.Query()
+
+	if len(tag) < 1 || len(tag) > 255 {
+		return fmt.Errorf("Tags must be between 1 and 255 characters in length")
+	}
+	q.Set("tag", tag)
+
+	u.RawQuery = q.Encode()
+
+	_, err = p.Get(u.String())
+	if err != nil {
+		return fmt.Errorf("Error from DeleteTag request %v", err)
+	}
+
+	return nil
+}
+
 type TagSuggestions struct {
 	XMLName     xml.Name `xml:"suggested"`
 	Popular     []string `xml:"popular"`
