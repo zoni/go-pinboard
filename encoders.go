beab92e2fe6d507900757ddae1be60a93fe683a9
Tim Sogard <src@timsogard.com>
2019-11-17 23:13:50 -0500

    JSON Marshalers for custom types
    Go's encoding/json encodes values of unknown types by calling their
    encoding.TextMarshal method, which produces default string
    representation for things like time.Time or []string. Explicitly
    defining the json Marshaler (which can be as simple as calling
    json.Marshal on a type conversion) produces the desired format.

--- encoders.go
+++ encoders.go
@@ -2,6 +2,7 @@ package pinboard
 
 import "strings"
 import "time"
+import "encoding/json"
 
 // postTags is a type for parsing tags returned by the Pinboard API. The API returns
 // tags as a space delimeted list, however we want to return them to the user as a
@@ -22,6 +23,10 @@ func (t *postTags) UnmarshalText(text []byte) error {
 	return nil
 }
 
+func (t postTags) MarshalJSON() ([]byte, error) {
+	return json.Marshal([]string(t))
+}
+
 // utcDate is a type for parsing _some_ of the dates returned by the Pinboard API.
 type utcDate struct {
 	time.Time
@@ -36,6 +41,11 @@ func (u *utcDate) UnmarshalText(text []byte) error {
 	d, err := time.Parse("2006-01-02", string(text))
 	*u = utcDate{d}
 	return err
+}
+
+func (u utcDate) MarshalJSON() ([]byte, error) {
+	s := u.UTC().Format("2006-01-02")
+	return []byte(s), nil
 }
 
 // notesDate is a type for parsing the datetime stamps in the notes list
